438b4b034700b09b254b3af674cca498bd0a0b34
Rizkyyudistira12 <mrizkyyudistira@gmail.com>
2023-04-06 09:35:54 +0700

    first-commit

---
+++ fungsi.js
@@ -0,0 +1,12 @@
+const add = (a, b) => a + b;
+console.log(add(1,2));
+
+const subtract = (a, b) => a - b;
+console.log(subtract(5,2));
+
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 2));
+
+const divide = (a, b) => a / b;
+console.log(divide(4, 2));
+
